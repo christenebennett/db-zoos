815e7780f01b295abe4bf7f74b3571ab52cca5be
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-14 20:37:24 -0700

    Add POST functionality

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const knex = require('knex');
 
 const knexConfig = {
   client: 'sqlite3',
-  connection: {
-    filename: './data/lambda.sqlite3',
-  },
+  connection: { filename: './data/lambda.sqlite3' },
   useNullAsDefault: true // this is needed for sqlite
 }
 const db = knex(knexConfig);
@@ -18,18 +16,29 @@ server.use(helmet());
 
 // endpoints here
 
-// list all zoos
-server.get('/api/zoos'), async (req, res) => {
-   try {
-    const zoos = await db('zoos');
-    res.status(200).json(zoos);
-   } catch (error) {
-    res.status(500).json(error);
-   }
-}
+// POST endpoint
+server.post('/api/zoos',  (req, res) => {
+  const newZoo = req.body;
+
+  if (newZoo) {
+    db.insert(newZoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(200).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  } else {
+    res.status(500).json({err: "Please provide name of zoo"});
+  }
+  
+})
 
 
-const port = 3300;
+
+
+const port = 5000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
